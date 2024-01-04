37c624536f6995c370ff71307a6b80305f4594f2
chlvldzmf <chlvldzmf@gmail.com>
2024-01-04 18:47:02 +0900

    abstract 연습

---
+++ Thisjava/src/sub7/PhoneExam.java
@@ -0,0 +1,14 @@
+package sub7;
+//326
+public class PhoneExam {
+	public static void main(String[] args) {
+		
+		SmartPhone smartPhone = new SmartPhone("홍길동");
+		
+		smartPhone.turnOn();
+		smartPhone.internetSearch();
+		smartPhone.turnOff();
+		
+	}
+
+}
